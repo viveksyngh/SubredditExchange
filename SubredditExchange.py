a967f5e65397864cc7132f5c3f009fe8ea930896
Karl Dreher <karldreher@gmail.com>
2017-06-26 23:07:07 -0700

    Closer!
    Reddit actually prompts for authorization.

--- SubredditExchange.py
+++ SubredditExchange.py
@@ -4,8 +4,8 @@ from flask import render_template
 
 
 app_client_id=''
-app_client_secret='',
-app_redirect_uri='http://localhost:8080',
+app_client_secret=''
+app_redirect_uri='http://localhost:8080/reddit_callback',
 app_user_agent='subreddit_exchange_testing by /u/karldreher'
 
 reddit = praw.Reddit(client_id=app_client_id,
@@ -15,8 +15,9 @@ reddit = praw.Reddit(client_id=app_client_id,
 
 
 #figure out what "random" means
-#scope may need to include subscribe in the future.  For now, get the "read" working.  
-authorize_url = "https://www.reddit.com/api/v1/authorize?client_id=" + app_client_id+"&response_type=code&state=RANDOM_STRING&redirect_uri=http://127.0.0.1:5000&duration=temporary&scope=mysubreddits"
+#scope may need to include subscribe in the future.  For now, get the "read" working.
+#fix redirect
+authorize_url = "https://www.reddit.com/api/v1/authorize?client_id=" + app_client_id+"&response_type=code&state=RANDOM_STRING&redirect_uri=http://localhost:8080/reddit_callback&duration=temporary&scope=mysubreddits"
 
 
 
@@ -27,6 +28,11 @@ def subreddit_exchange_app():
     return render_template('index.html', url=authorize_url)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=8080)
+
+@app.route("/reddit_callback")
+def Auth():
+    return "You are authorized"
+    
 
     
